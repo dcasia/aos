1612d96d1ecc39726d42c778b62939c3d2ac9794
RyanWu <toryanwu@gmail.com>
2020-02-24 20:02:43 +0800

    handle conflict

--- src/js/libs/offset.js
+++ src/js/libs/offset.js
@@ -6,24 +6,21 @@
  * @return {Object} [top and left offset]
  */
 const offset = function(el) {
-  let _x = 0;
-  let _y = 0;
-
-  while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
-    _x += el.offsetLeft - (el.tagName != 'BODY' ? el.scrollLeft : 0);
-    _y += el.offsetTop - (el.tagName != 'BODY' ? el.scrollTop : 0);
-    el = el.offsetParent;
-  }
-
-  return {
-    top: _y,
-    left: _x
-  };
-
-//   const { top, left } = el.getBoundingClientRect()
-
-//   return { top, left }
+    let _x = 0;
+    let _y = 0;
+  
+    while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
+      _x += el.offsetLeft - (el.tagName != 'BODY' ? el.scrollLeft : 0);
+      _y += el.offsetTop - (el.tagName != 'BODY' ? el.scrollTop : 0);
+      el = el.offsetParent;
+    }
+  
+    return {
+      top: _y,
+      left: _x
+    };
 
 };
 
 export default offset;
+  
